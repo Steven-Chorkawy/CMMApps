76fcf8e97f90dd8a7ea1e5e304f41c141247a8ce
Steven Chorkawy <schorkawy@clarington.net>
2022-02-09 18:05:34 -0500

    hello.

--- src/ClaringtonComponents/MemberDetailsComponent.tsx
+++ src/ClaringtonComponents/MemberDetailsComponent.tsx
@@ -8,48 +8,15 @@ import NewMemberForm from './NewMemberForm';
 
 
 export interface IMemberDetailsComponentProps {
-    isOpen?: boolean;
-    panelType?: PanelType;
-    context: any;
+
 }
 
 export default class MemberDetailsComponent extends React.Component<IMemberDetailsComponentProps, any> {
     constructor(props) {
         super(props);
-
-        this.state = {
-            isOpen: this.props.isOpen,
-            selectedForm: undefined
-        };
     }
 
     public render(): React.ReactElement<any> {
-        let wrapper = undefined;
-        return (
-            <Panel
-                isLightDismiss={false}
-                isOpen={this.state.isOpen}
-                type={this.props.panelType ? this.props.panelType : PanelType.custom}
-                onDismiss={() => this.setState({ isOpen: !this.state.isOpen })}
-                customWidth={'800px'}
-            >
-                <div ref={e => wrapper = e}>
-                    <Dropdown
-                        label="Add New or Existing Member"
-                        options={[{ key: "Add New Member", text: "Add New Member" }, { key: "Add Existing Member", text: "Add Existing Member" }]}
-                        onChange={(event, options) => {
-                            console.log(options);
-                            this.setState({ selectedForm: options.key });
-                        }}
-                    />
-                    <Separator />
-                    {
-                        this.state.selectedForm === "Add New Member" ?
-                            <NewMemberForm context={this.props.context} description='???' /> :
-                            <AddCommitteeMemberForm context={this.props.context} description={'???'} />
-                    }
-                </div>
-            </Panel >
-        );
+        return (<span>hello</span>);
     }
 }
